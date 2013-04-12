eb0860f40b675d89d24105a368ed0ff795c5b818
Martin Silva <martinsilva@zauberlabs.com>
2013-04-12 11:17:06 -0300

    initial commmit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def index
+    r = nil
+
+    response = RestClient.post 'https://crono.zauberlabs.com/crono-web-0.14/bin/login/check', 
+    :j_username => '', :j_password => '', :submit => 'Login' do |response, request, result, &block|
+      if(response.code == 302 and not response.headers[:location].include? 'bin/login')
+        cookies = response.headers[:set_cookie][0].split ";"
+        j_session = cookies[0];
+        render :json => get_users(j_session)
+      else
+        throw new Error("Login failed!");
+      end  
+    end
+
+  end
+
+
+  private 
+
+  def get_users j_session_cookie
+    response = RestClient.get("https://crono.zauberlabs.com/crono-web-0.14/api/zauber/people/", :Cookie => j_session_cookie)
+    response.body
+  end  
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PingPong::Application.config.secret_token = 'e84699de024b4239f5c5025e669fc01258e89ad79d6274b458da739cb7f02cefd246506b60ce0c528686bf92e9cffefcebe6677b89c82925694dc4086f7325f2'
